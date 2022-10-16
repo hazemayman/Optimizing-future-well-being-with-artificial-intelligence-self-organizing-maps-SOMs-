f456ec19c6c143f364b3e30d0437fc9bf702cb49
hazemayman <hazemhany09@gmail.com>
2022-10-16 10:14:42 +0200

    added model_training jupyter notebook with front-end interface and API for inference

---
+++ webbapp/wellbeingapp/api/api.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__ )
+from script import infeere
+import io
+from base64 import encodebytes
+from PIL import Image
+
+
+# the endpoint that front end uses to send the answeres for the Questions
+@app.route('/api/v0/SendData', methods=['POST','GET'])
+def get_current_time():
+    print("here")
+    data= request.get_json()['Data']
+    result , BMU = infeere(data) # get the results of the Ryff diemensions , as wel la the best matching unit in the self organzing map for the current depression level
+    print(result , BMU)
+
+    # here all we do is that we convert the numpy datatype to integer after rounding the floating point and converting it to int
+    float_list =  list(result[0])
+    round_list = []
+    for i in float_list :
+        round_list.append(int(round(i))) 
+
+    return {"result" : round_list , "BMU" : [int(BMU[0]) ,int(BMU[1])] } # jsonify the data and send it back in json format to the client (React application)
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
---
+++ webbapp/wellbeingapp/src/App.js
@@ -0,0 +1,97 @@
+
+import './App.css';
+import Question from './Components/Question'; 
+import React  , {useState , useEffect} from 'react'
+import {
+  Routes ,
+  Route,
+} from "react-router-dom";
+import output from './output.png'; 
+import axios from 'axios';
+
+
+
+function App() {
+  let [questionAnswerd , setQuestionAnswered] = useState(false) // bool state that determines to view the questions or the results 
+  let [info , setInfo] = useState({}) // state holds the best matching unit as well as the 6 dimensions of the Ryff method
+  /* 
+    this function is getting called when the user submit the answers
+    the function takes the answer and make a post request to the proxy (localhos twith port 5000 flask application backend)
+    the endpoint will infeere using the model and the SOM and return the values and update the info state using these values 
+    then aafter all this completed the questionAnswerd bool state will be true to view the results and unmount the Question Component
+
+  */
+  const sendData = (quesitons_answers) =>{
+    console.log(quesitons_answers)
+      axios.post('/api/v0/SendData', {
+        'Data': quesitons_answers,
+      }).then(function (response){
+        console.log(response.data)
+        setInfo({'wellbeing' : response.data.result , 'BMU' : response.data.BMU})
+        setQuestionAnswered(true)
+      })
+   
+  }
+
+  // used this useEffect just for testing purposes 
+  useEffect(()=>{
+    if(JSON.stringify(answers) !== '{}'){
+      console.log(answers)
+    }
+  } , [answers])
+  
+  let questionBlock =   <div className='main-container' >
+                          <Question sendData = {sendData}/>
+                        </div>
+  return (
+    <>
+    <Routes>
+          {(!questionAnswerd) &&<Route exact path="/" element = {questionBlock}/>}
+          {(!questionAnswerd) && <Route exact path="*" element = {questionBlock}/>}   
+
+            {(questionAnswerd) &&<Route exact path="/" element = {
+              <>
+             
+              <div className='result-container'>
+              <h1 className='header'>Future well-being Ryff dimensions</h1>
+                <table>
+                  <tr>
+                    <td>Positive relations</td>
+                    <td>{info['wellbeing'][0]}</td>
+                  </tr>
+                  <tr>
+                    <td>Self-acceptance </td>
+                    <td>{info['wellbeing'][1]}</td>
+                  </tr>
+                  <tr>
+                    <td>Autonomy</td>
+                    <td>{info['wellbeing'][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>Personal growth </td>
+                    <td>{info['wellbeing'][3]}</td>
+                  </tr>
+                  <tr>
+                    <td>Environmental mastery</td>
+                    <td>{info['wellbeing'][4]}</td>
+                  </tr>
+                  <tr>
+                    <td>Purpose in life</td>
+                    <td>{info['wellbeing'][5]}</td>
+                  </tr>
+
+                </table>
+
+                <hr></hr>
+                 <p className='text'>the Current depression level (row , column) -> {'('+ info['BMU'][0]+' , '+ info['BMU'][1]+ ')'}</p>
+                 <img  className='som-image' src={output} alt="the output SOM" />
+                <hr></hr>
+              </div>
+              </>
+            }/>}       
+    </Routes>
+    </>
+  );
+}
+
+export default App;
